4db5560dccb5b379278565d508b472a442ebfaf9
Pablo Gauna <gaunapablo88@hotmail.com>
2013-04-24 02:40:28 -0300

    Agrege una funcionalidad
    funcionalidad para que no se pueda reservar en un vuelo si se supera la
    cantidad de asientos permitidos para esa clase en algun vuelo de la
    reserva....
    
    por favor vean la solucion porque no estoy tan ducho en el SQL :P

--- tp1/Informe/terceraConsulta.sql
+++ tp1/Informe/terceraConsulta.sql
@@ -1,1 +1,62 @@
-CREATE PROCEDURE 
+CREATE TRIGGER RestringirCupo ON Reservas
+	BEFORE INSERT
+	FOR EACH ROW
+
+	BEGIN
+
+	IF( SELECT COUNT(*) FROM ViajesConEscalas ve
+		WHERE ve.idVueloConEscalas = :NEW.idVueloConEscalas
+		AND 
+		EXISTS -- exista algun vuelo
+		(SELECT * FROM vuelos v 
+			WHERE -- tal que
+			( -- el vuelo pertenezca al vuelo con escala
+				v.idVuelo = ve.idViajePartida
+				OR v.idVuelo = ve.idViajeLLegada
+				OR EXISTS  
+					(SELECT * FROM HaceEscalaEn he
+						WHERE he.idVueloConEscalas = ve.idVueloConEscalas
+						AND v.idVuelo = he.idVuelo
+					)
+			)
+			AND
+			( -- y ese vuelo no tenga mas asientos
+				EXISTS
+				(SELECT * FROM DisponeDeAsientos da
+					WHERE v.idAeronave = da.idAeronave
+					AND da.idClase = :NEW.idClase
+					AND da.asientos > 
+						( -- contar asientos ocupados de un vuelo/clase
+						SELECT COUNT(*) FROM reservas r
+							WHERE r.idClase = :new.idClase
+							AND 
+							(
+								EXISTS
+									(SELECT * FROM HaceEscalaEn he2
+										WHERE v.idVuelo = he2.idVuelo	
+										AND he2.idVueloConEscalas = 
+												r.idVueloConEscalas
+									) 
+								OR EXISTS 
+									(SELECT * FROM ViajesConEscalas ve2
+										WHERE ve2.idVueloConEscalas = 
+												r.idVueloConEscalas
+										AND 
+										(
+										 v.idVuelo = ve2.idViajePartida 
+										 OR v.idVuelo = ve2.idViajeLLegada 
+										)
+									)
+							)
+						)
+				)
+			)
+		)
+	> 0)
+		BEGIN
+			RAISEERROR ('No hay asientos sufientes')
+			ROLLBACK TRANSACTION
+		END
+	END
+
+
