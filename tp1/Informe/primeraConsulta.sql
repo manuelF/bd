185c1124006ffe2e0afac40440629f5cf0dd7173
Juan Pablo Darago <jpdarago@gmail.com>
2013-04-24 00:18:45 -0300

    Avance un toque con el tp

--- tp1/Informe/primeraConsulta.sql
+++ tp1/Informe/primeraConsulta.sql
@@ -1,16 +1,12 @@
 SELECT nombre, apellido, idUsuario FROM usuarios
 	WHERE idUsuario IN (
-	 SELECT u.idUsuario FROM usuarios u WHERE NOT EXISTS (
-	  SELECT p.idPais FROM paises p WHERE NOT EXISTS (
-        SELECT idReserva FROM reservas r
-        JOIN esReservaPara ep    ON r.idReserva = ep.idReserva
-        JOIN viajeConEscalas vce ON vce.idViajeConEscalas = 
-                                        ep.idViajeConEscalas
-        JOIN viaje v             ON v.idViaje = vce.idViajeLlegada
-        JOIN aeropuerto ap       ON v.llega = ap.idAeropuerto
-        JOIN ciudades c          ON c.idCiudad = ap.idCiudad
-            WHERE c.idPais = p.idPais 
-            AND   r.idUsuario = u.idUsuario
-	  )
-	 )
-	)
+		SELECT u.idUsuario FROM usuarios u WHERE NOT EXISTS (
+				SELECT p.idPais FROM paises p WHERE NOT EXISTS (
+					SELECT idReserva FROM reservas r, vuelosConEscala ve,
+						vuelos v, aeropuertos ap, ciudad c
+					WHERE r.idUsuario = u.idUsuario	
+						AND r.idVueloConEscala = ve.idVueloConEscala
+						AND ve.idVueloLlegada = v.idVuelo
+						AND ap.idAeropuerto = v.idAeropuertoLlegada
+						AND c.idCiudad = ap.idCiudad
+						AND c.idPais = p.idPais	)))
--- tp1/Informe/segundaConsulta.sql
+++ tp1/Informe/segundaConsulta.sql
@@ -7,32 +7,80 @@ CREATE TRIGGER RestringirReservasUsuario ON Reservas
 	DECLARE fechaSalida AS DATE
 	DECLARE fechaLlegada AS DATE
 					
-	-- Conseguimos las fechas de salida y llegada
-	SELECT v.fechaSalida INTO fechaSalida 
-		FROM vuelos v,vuelosConEscalas ve, EsReservaDe er
-			WHERE v.idVuelo = ve.idVueloPartida
-			AND ve.idVueloConEscalas = er.idVueloConEscalas
-			AND er.idReserva = :NEW.idReserva
+	DECLARE idAeropuertoSalida	AS INTEGER
+	DECLARE idAeropuertoLlegada AS INTEGER
 
-	SELECT v.fechaLlegada INTO fechaLlegada
-	FROM vuelos v,vuelosConEscalas ve, EsReservaDe er
-	WHERE v.idVuelo = ve.idVueloLlegada
-	AND ve.idVueloConEscalas = er.idVueloConEscalas
-	AND er.idReserva = :NEW.idReserva
-				
-	-- Nos fijamos si una reserva se superpone
-	IF( SELECT COUNT(*) FROM reservas WHERE EXISTS
-		( SELECT * FROM vuelos v,vuelosConEscala ve,
-			EsReservaPara ep,Reservas r, Usuario u
+	-- Conseguimos las fechas y aeropuertos 
+	-- de salida y llegada
+	SELECT v.fechaSalida, v.aeropuertoSalida INTO 
+		fechaSalida, idAeropuertoSalida
+		FROM vuelos v,vuelosConEscalas ve, reservas
+			WHERE v.idVuelo = ve.idVueloPartida
+			AND ve.idVueloConEscalas = :NEW.idViajeConEscalas
+
+	SELECT v.fechaLlegada, v.idAeropuertoLlegada INTO 
+		fechaLlegada,idAeropuertoLlegada
+		FROM vuelos v,vuelosConEscalas ve, reservas
+			WHERE v.idVuelo = ve.idVueloLlegada
+			AND ve.idVueloConEscalas = :NEW.idViajeConEscalas
+
+	-- Conseguimos el aeropuerto de llegada y el de salida
+	
+	-- Nos fijamos si una reserva con distinto aeropuerto se superpone 	
+	SELECT r.idReserva AS idReserva
+		FROM reservas r, vuelos v,vuelosConEscalas ve,haceEscalaEn he
 			WHERE v.fechaSalida <= fechaSalida
 				AND v.fechaLlegada >= fechaLlegada
-				AND v.
-		)
-	> 0)
-		BEGIN
-						
-
-			RAISEERROR ('Restriccion de reservas violada')
+				AND he.idVuelo = v.idVuelo
+				AND he.idVueloConEscalas = r.idVueloConEscalas
+				AND r.idReserva = :NEW.idUsuario
+		MINUS
+		SELECT r.idReserva AS idReserva
+			FROM reservas r, vuelos v, haceEscalaEn he
+			WHERE v.idAeropuertoSalida = idAeropuertoSalida
+				AND v.idVuelo = he.idVuelo
+				AND v.idVueloConEscalas = r.idVueloConEscalas
+		 INTERSECT
+		 SELECT r.idReserva AS idReserva
+			FROM reservas r, vuelos v, haceEscalaEn he
+			WHERE v.idAeropuertoLlegada = idAeropuertoLlegada
+				AND v.idVuelo = he.idVuelo
+				AND v.idVueloConEscalas = r.idVueloConEscalas
+	
+	IF(@@ROWCOUNT > 0)
+		BEGIN 
+			RAISEERROR ('Restriccion de reservas violada: Reservas se superponen')
 			ROLLBACK TRANSACTION
+		END
+	
+	-- Nos fijamos si hay mas de 2 reservas con esa fecha de partida igual aeropuerto
+	SELECT r.idReservas
+		FROM reservas r,vuelos v,vuelosConEscala ve
+		WHERE r.idVuelosConEscalas = ve.idVueloConEscalas
+			AND v.idVuelo = ve.idVueloPartida
+			AND v.idAeropuertoLlegada = idAeropuertoLlegada
+			AND v.fechaSalida = fechaSalida
+	INTERSECT
+	SELECT r.idReservas
+   		FROM reservas r,vuelos v,vuelosConEscala ve
+		WHERE r.idVueloConEscalas = ve.idVueloConEscalas
+			AND v.idVuelo = ve.idVueloPartida
+			AND v.idAeropuertoLlegada = idAeropuertoLlegada
+			AND v.fechaLlegada = fechaLlegada
+	
+	if @@ROWCOUNT > 1
+		BEGIN
+			RAISEERROR ('Restriccion de reservas violada: Mas de dos reservas por aeropuerto')
+			ROLLBACK TRANSACTION
+		END
+
+	-- Nos fijamos si, habiendo una reserva, la reserva tiene fecha de partida en los proximos dias	
+	IF @@ROWCOUNT = 1
+		BEGIN
+			IF fechaSalida >= DATEADD(fechaSalida,DATEADD(day,7,GETDATE()))
+			BEGIN
+				RAISEERROR('Restriccion de reservas violada: La reserva es para dentro de mas de 7 dias')
+				ROLLBACK TRANSACTION
+			END
 		END
 	END
