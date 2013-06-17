6ca9b0bb6ad13fdc1f0b4e2677124271ca9add73
Juan Pablo Darago <jpdarago@gmail.com>
2013-06-17 02:32:33 -0300

    Factorizado el Main Evaluator, igual se podría hacer más limpieza general. Ahora veo de crear algo para hacer tests mas facil

--- tp2/UBADB/src/main/java/ubadb/external/bufferManagement/MainEvaluator.java
+++ tp2/UBADB/src/main/java/ubadb/external/bufferManagement/MainEvaluator.java
@@ -4,18 +4,13 @@ import ubadb.core.components.bufferManager.BufferManager;
 import ubadb.core.components.bufferManager.BufferManagerException;
 import ubadb.core.components.bufferManager.BufferManagerImpl;
 import ubadb.core.components.bufferManager.bufferPool.BufferPool;
-import ubadb.core.components.bufferManager.bufferPool.pools.single.SingleBufferPool;
 import ubadb.core.components.bufferManager.bufferPool.replacementStrategies.PageReplacementStrategy;
 import ubadb.core.components.bufferManager.bufferPool.replacementStrategies.fifo.FIFOReplacementStrategy;
 import ubadb.core.components.bufferManager.pools.multiple.MultipleBufferPool;
 import ubadb.core.components.catalogManager.CatalogManager;
 import ubadb.core.components.catalogManager.CatalogManagerImpl;
 import ubadb.core.components.diskManager.DiskManager;
-import ubadb.external.bufferManagement.etc.BufferManagementMetrics;
-import ubadb.external.bufferManagement.etc.FaultCounterDiskManagerSpy;
-import ubadb.external.bufferManagement.etc.PageReference;
-import ubadb.external.bufferManagement.etc.PageReferenceTrace;
-import ubadb.external.bufferManagement.etc.PageReferenceTraceSerializer;
+import ubadb.external.bufferManagement.etc.*;
 
 import java.io.File;
 import java.util.HashMap;
@@ -27,29 +22,33 @@ public class MainEvaluator {
 	public static void main(String[] args) {
 		try {
 			PageReplacementStrategy pageReplacementStrategy = new FIFOReplacementStrategy();
-            String  traceFileName = "generated/mixed-filescans-clustered.trace";
-                    traceFileName = "generated/mixed-filescans.trace";
-			int bufferPoolSize = 100;
+            //String  traceFileName = "generated/mixed-filescans-clustered.trace";
+            String traceFileName2 = "generated/mixed-filescans.trace";
 
-			evaluate(pageReplacementStrategy, traceFileName, bufferPoolSize);
+            String currentPath = new File(".").getAbsoluteFile() + "/src/test/resources/catalogs/";
+            CatalogManager catalogManager = new CatalogManagerImpl("salesCatalog.catalog",currentPath);
+
+            Map<String,Integer> pages = new HashMap<>();
+            pages.put("KEEP",20);
+            pages.put("DEFAULT",150);
+            pages.put("RECYCLE",120);
+
+            BufferPool bp = createMultipleBufferPool(catalogManager,pageReplacementStrategy,pages);
+
+            evaluate(traceFileName2, catalogManager, bp);
 		} catch (Exception e) {
 			System.out.println("FATAL ERROR (" + e.getMessage() + ")");
 			e.printStackTrace();
 		}
 	}
 
-	private static void evaluate(
-			PageReplacementStrategy pageReplacementStrategy,
-			String traceFileName, int bufferPoolSize) throws Exception,
-			InterruptedException, BufferManagerException {
-		
-		FaultCounterDiskManagerSpy faultCounterDiskManagerSpy = new FaultCounterDiskManagerSpy();
-        String currentPath = new File(".").getAbsoluteFile() + "/src/test/resources/catalogs/";
-        CatalogManager catalogManager = new CatalogManagerImpl("salesCatalog.catalog",currentPath);
+	private static void evaluate(String traceFileName,
+                                 CatalogManager catalogManager,
+                                 BufferPool bufferPool) throws Exception {
+        FaultCounterDiskManagerSpy faultCounterDiskManagerSpy = new FaultCounterDiskManagerSpy();
 
-		BufferManager bufferManager = createBufferManager(
-				faultCounterDiskManagerSpy, catalogManager,
-				pageReplacementStrategy, bufferPoolSize);
+        BufferManager bufferManager = createBufferManager(faultCounterDiskManagerSpy,
+                catalogManager,bufferPool);
 		PageReferenceTrace trace = getTrace(traceFileName);
 
 		for (PageReference pageReference : trace.getPageReferences()) {
@@ -73,7 +72,7 @@ public class MainEvaluator {
 			}
 		}
 
-		BufferManagementMetrics metrics = new BufferManagementMetrics(trace,
+        BufferManagementMetrics metrics = new BufferManagementMetrics(trace,
 				faultCounterDiskManagerSpy.getFaultsCount());
 		metrics.showSummary();
 	}
@@ -85,16 +84,8 @@ public class MainEvaluator {
 	}
 
 	private static BufferManager createBufferManager(DiskManager diskManager,
-			CatalogManager catalogManager,
-			PageReplacementStrategy pageReplacementStrategy, int bufferPoolSize) {
-		/*BufferPool singleBufferPool = new SingleBufferPool(bufferPoolSize,
-				pageReplacementStrategy);                                   */
-        Map<String,Integer> pages = new HashMap<>();
-        pages.put("KEEP",20);
-        pages.put("DEFAULT",150);
-        pages.put("RECYCLE",120);
-        BufferPool multipleBufferPool = new MultipleBufferPool(pages,"DEFAULT",
-                new FIFOReplacementStrategy(),catalogManager);
+			CatalogManager catalogManager,BufferPool bufferPool) {
+
         try
         {
             catalogManager.loadCatalog();
@@ -104,9 +95,13 @@ public class MainEvaluator {
             System.err.println("Cannot load catalog!");
         }
 
-		BufferManager bufferManager = new BufferManagerImpl(diskManager,
-				catalogManager, multipleBufferPool);
-
-		return bufferManager;
+		return new BufferManagerImpl(diskManager,catalogManager, bufferPool);
 	}
+
+    private static BufferPool createMultipleBufferPool(CatalogManager catalogManager,
+                                                       PageReplacementStrategy pageReplacementStrategy,
+                                                       Map<String, Integer> pages) {
+        return new MultipleBufferPool(pages,"DEFAULT",
+                    pageReplacementStrategy,catalogManager);
+    }
 }
